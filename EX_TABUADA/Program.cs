5f15b9a6c7833ce0c0ef33f3b9c4789b17327a6d
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-04 23:37:56 -0300

    primeiro commit

---
+++ EX_TABUADA/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EX_TABUADA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada;
+            int i;
+
+            Console.WriteLine("Qual tabuada você deseja?");
+            entrada = Console.ReadLine();
+            int.TryParse(entrada, out int n);
+
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n*i}");
+            }
+        }
+    }
+}
